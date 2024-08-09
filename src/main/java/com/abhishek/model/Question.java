e66869d339ab4ada01897b3687b194610616f20c
Abhishek Dumaniya <abhishekdumaniya196@gmail.com>
2024-08-09 21:17:45 +0530

    initial commit

---
+++ src/main/java/com/abhishek/model/Question.java
@@ -0,0 +1,24 @@
+package com.abhishek.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Question {
+
+    @Id
+    private Integer id;
+    private String QuestionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String difficultylevel;
+    private String category;
+
+}
