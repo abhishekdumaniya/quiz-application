c81f5ec16d2907b1dbe1de3e0b0caa60e627fc6e
Abhishek Dumaniya <abhishekdumaniya196@gmail.com>
2024-08-13 21:23:21 +0530

    initial commit

---
+++ src/main/java/com/abhishek/service/QuestionService.java
@@ -0,0 +1,41 @@
+package com.abhishek.service;
+
+import com.abhishek.model.Question;
+import com.abhishek.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestion() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_GATEWAY);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_GATEWAY);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+}
